59c848ac82ca0e61dce8461cac92159edf428be1
MrSemen <128792149+assteven@users.noreply.github.com>
2023-04-25 13:38:25 +0200

    Buenas Bbs
    Cambiadísimo al reto

--- HolaBuenosDias.sh
+++ HolaBuenosDias.sh
@@ -1,28 +1,79 @@
 #!/bin/bash
 
-# Solicitamos la ruta del directorio
-read -p "Introduce la ruta del directorio: " ruta_directorio
+dentro=true
 
-# Validamos que la ruta introducida sea un directorio
-if [ ! -d "$ruta_directorio" ]; then
-  echo "La ruta introducida no es un directorio válido"
-  exit 1
-fi
+# Funciones que van a hacer la gestión de usuarios
 
-# Contadores para archivos y directorios
-num_archivos=0
-num_directorios=0
+# Función Mostrar usuarios
+function mostrar_usuarios() {
+  echo "Los usuarios en el sistema son:"
+  cut -d: -f1 /etc/passwd
+}
 
-# Recorremos el directorio y contamos archivos y directorios
-for elemento in "$ruta_directorio"/*; do
-  if [ -f "$elemento" ]; then
-    ((num_archivos++))
-  elif [ -d "$elemento" ]; then
-    ((num_directorios++))
-  fi
+# Función información de un usuario
+function informacion_un_usuario() {
+  echo "Ingrese el nombre del usuario: "
+  read nombre_usuario
+  echo "La información para el usuario $nombre_usuario es:"
+  id $nombre_usuario
+  groups $nombre_usuario
+}
+
+# Función añadir un nuevo usuario
+function anadir_usuario() {
+  echo "Ingrese el nombre del nuevo usuario: "
+  read nombre_usuario
+  sudo adduser $nombre_usuario
+}
+
+# Función modificar un usuario
+function modificar_usuario() {
+  echo "Ingrese el nombre del usuario a modificar: "
+  read nombre_usuario
+  sudo usermod -aG sudo $nombre_usuario
+}
+
+echo "RETO DE GESTIÓN DE USUARIOS "
+
+# Bucle Principal
+while $dentro;
+do
+  # Pregunto al usuario cositas
+  echo -e "\n\n---------- GESTIÓN DE USUARIOS -----------\n"
+  echo "1- Ver todos los usuarios"
+  echo "2- Información de un usuario"
+  echo "3- Añadir un nuevo usuario"
+  echo "4- Modificar usuario"
+  echo "0- Salir"
+  echo "Ingresa un número: "
+  read numero
+
+  # Hacer cada función de gestión de usuarios
+  case $numero in
+    1)
+      echo "Ver todos los usuarios:"
+      mostrar_usuarios
+      ;;
+    2)
+      echo "Información de un usuario en concreto"
+      informacion_un_usuario
+      ;;
+    3)
+      echo "Añadir un usuario nuevo"
+      anadir_usuario
+      ;;
+    4)
+      echo "Modificar un usuario"
+      modificar_usuario
+      ;;
+    0)
+      # Salir de la aplicación
+      echo "SALIENDO DE LA APLICACIÓN..."
+      dentro=false
+      ;;
+    *)
+      echo "Selecciona un valor correcto!"
+      ;;
+  esac
+
 done
-
-# Mostramos la información por pantalla
-echo "En el directorio $ruta_directorio:"
-echo "Número de archivos: $num_archivos"
-echo "Número de directorios: $num_directorios"
